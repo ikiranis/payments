325a4a3453910cfa6a901429b7e1b4700e43d6fc
rocean <rocean74@gmail.com>
2022-05-26 02:27:16 +0300

    Remove months

--- src/Main.java
+++ src/Main.java
@@ -7,11 +7,11 @@ public class Main {
         c.addEmployee(new Analyst("VV", new PerHour(15)));
         c.addEmployee(new Technical("CC", new Salary()));
         c.addEmployee(new Developer("KK", new Salary()));
-        c.addProjectToEmployee("AA", new DevelopmentProject("Website for UoM", 4));
-        c.addProjectToEmployee("BB", new DevelopmentProject("Website for UoM ", 4));
-        c.addProjectToEmployee("VV", new DevelopmentProject("Website for UoM ", 4));
+        c.addProjectToEmployee("AA", new DevelopmentProject("Website for UoM"));
+        c.addProjectToEmployee("BB", new DevelopmentProject("Website for UoM "));
+        c.addProjectToEmployee("VV", new DevelopmentProject("Website for UoM "));
         c.addProjectToEmployee("CC", new TechnicalProject("Network setup for EAP"));
-        c.addProjectToEmployee("KK", new DevelopmentProject("Website for UoM", 4));
+        c.addProjectToEmployee("KK", new DevelopmentProject("Website for UoM"));
         c.addProjectToEmployee("BB", new TechnicalProject("Network setup for EAP"));
         c.calcPayroll();
     }
