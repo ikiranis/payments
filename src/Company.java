2a4eed40ac647d1b9f8f08c68ae8ab23590f2b77
rocean <rocean74@gmail.com>
2022-05-27 03:30:05 +0300

    Print text

--- src/Company.java
+++ src/Company.java
@@ -28,11 +28,11 @@ public class Company {
         // Εκτύπωση μισθού για κάθε υπάλληλο και υπολογισμός του
         // συνολικού ποσού μισθοδοσίας για την εταιρεία
         for(Employee employee : employeeList) {
-            System.out.println("Name: " + employee.getName() + " Salary: " + employee.getMonthlySalary() + " ευρώ");
+            System.out.println("Name: " + employee.getName() + " Salary: " + employee.getMonthlySalary() + " euro");
             salarySum += employee.getMonthlySalary();
         }
 
-        System.out.println("Total monthly salaries: " + salarySum);
+        System.out.println("Total monthly salaries: " + salarySum + " euro");
 
     }
 
