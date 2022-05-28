77b468ae2f4f6b633d54d94e7e88e7c6b90e1e59
rocean <rocean74@gmail.com>
2022-05-28 03:57:19 +0300

    Remove comments

--- src/Main.java
+++ src/Main.java
@@ -116,24 +116,10 @@ public class Main {
     }
 
     public static void main(String[] args) {
-
         addEmployees();
 
         addMonthlyPayroll();
 
         c.calcPayroll();
-
-//        c.addEmployee(new Developer("AA", new Salary()));
-//        c.addEmployee(new Manager("BB", new Salary()));
-//        c.addEmployee(new Analyst("VV", new PerHour(15)));
-//        c.addEmployee(new Technical("CC", new Salary()));
-//        c.addEmployee(new Developer("KK", new Salary()));
-//        c.addProjectToEmployee("AA", new DevelopmentProject("Website for UoM"));
-//        c.addProjectToEmployee("BB", new DevelopmentProject("Website for UoM "));
-//        c.addProjectToEmployee("VV", new DevelopmentProject("Website for UoM "));
-//        c.addProjectToEmployee("CC", new TechnicalProject("Network setup for EAP"));
-//        c.addProjectToEmployee("KK", new DevelopmentProject("Website for UoM"));
-//        c.addProjectToEmployee("BB", new TechnicalProject("Network setup for EAP"));
-
     }
 }
